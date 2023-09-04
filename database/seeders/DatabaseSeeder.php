563b752ca31fbcac326adeea7d2ada3b00a1f8f3
Giovane <giovanelr@gmail.com>
2023-09-04 16:40:38 -0300

    [OC-02] Adicionado migration e model para a conta

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\Conta::factory()->create([
+            'username' => fake()->name(),
+            'saldo' => 500.00,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
