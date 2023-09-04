77d53c17e717b5a0212623d86c27437f138031c2
Giovane <giovanelr@gmail.com>
2023-09-04 17:22:26 -0300

    [OC-02] Adicionado rotas e funções de listar, inserir e atualizar contas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/contas', [ContasController::class, 'index']);
+Route::get('/conta', [ContasController::class, 'index']);
+Route::get('/conta/{id}', [ContasController::class, 'show']);
+Route::put('/conta', [ContasController::class, 'store']);
+Route::patch('/conta/{id}', [ContasController::class, 'update']);
