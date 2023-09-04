1ff3e2acd1eb47a0fd6b7f7572f979262a0f5bd6
Giovane <giovanelr@gmail.com>
2023-09-04 16:47:14 -0300

    [OC-03] Adicionado migration e model para transacoes

---
+++ app/Http/Controllers/TransacoesController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTransacoesRequest;
+use App\Http\Requests\UpdateTransacoesRequest;
+use App\Models\Transacoes;
+
+class TransacoesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTransacoesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Transacoes $transacoes)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTransacoesRequest $request, Transacoes $transacoes)
+    {
+        //
+    }
+
+}
