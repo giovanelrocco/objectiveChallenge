94bad33e16e59415c86d6908103964240cb67746
Giovane Lucca Rocco <giovanelr@gmail.com>
2023-09-05 10:22:46 -0300

    [OC-05] Adicionado logica para descontar a taxa e testes para calcular cada taxa

--- app/Models/Transacoes.php
+++ app/Models/Transacoes.php
@@ -8,6 +8,14 @@ use Illuminate\Support\Facades\DB;
 
 class Transacoes extends Model
 {
+    const METODO_PAGAMENTO_CREDITO = "C";
+    const METODO_PAGAMENTO_DEBITO = "D";
+    const METODO_PAGAMENTO_PIX = "P";
+
+    const TAXA_METODO_PAGAMENTO_CREDITO = 0.05;
+    const TAXA_METODO_PAGAMENTO_DEBITO = 0.03;
+    const TAXA_METODO_PAGAMENTO_PIX = 0;
+
     use HasFactory;
 
     public static function saveTransacao(Transacoes $transacao)
@@ -16,7 +24,8 @@ class Transacoes extends Model
             DB::beginTransaction();
 
             $conta = new Conta;
-            $conta->updateSaldoConta($transacao->conta_id, $transacao->valor);
+            $saldo_debitar = $transacao->calcularSaldoDebitar($transacao->forma_pagamento, $transacao->valor);
+            $conta->updateSaldoConta($transacao->conta_id, $saldo_debitar);
             $transacao->save();
 
             DB::commit();
@@ -28,5 +37,27 @@ class Transacoes extends Model
             throw new \App\Exceptions\TransacoesException('Erro ao salvar a informação');
 
         }
+    }
+
+    public function calcularSaldoDebitar(string $forma_pagamento, float $valor_transacao): float
+    {
+        $saldo_debitar = $valor_transacao;
+        switch ($forma_pagamento) {
+            case Transacoes::METODO_PAGAMENTO_CREDITO:
+                $saldo_debitar += $valor_transacao * Transacoes::TAXA_METODO_PAGAMENTO_CREDITO;
+                break;
+            case Transacoes::METODO_PAGAMENTO_DEBITO:
+                $saldo_debitar += $valor_transacao * Transacoes::TAXA_METODO_PAGAMENTO_DEBITO;
+                break;
+            case Transacoes::METODO_PAGAMENTO_PIX:
+                $saldo_debitar = $valor_transacao;
+                break;
+
+            default:
+                throw new \App\Exceptions\TransacoesException('Método de pagamento não encontrado');
+                break;
+        }
+
+        return $saldo_debitar;
     }
 }
